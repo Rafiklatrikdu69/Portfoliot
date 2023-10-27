3efc0214f0397ec04f58858b4d67ffb829e77832
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2023-10-27 19:50:02 +0200

    Modification et style de la page

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = "Salut je m'appelle Rafik Bouchenna";
+const textContent = document.getElementById("text-content");
+const readMoreButton = document.getElementById("read-more-button");
+const restOfPage = document.getElementById("rest-of-page");
+
+let index = 0;
+
+function afficherTexteSequentiel() {
+  if (index < text.length) {
+    textContent.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(afficherTexteSequentiel, 50);
+  } else {
+    // Une fois que tout le texte a été affiché, affichez le bouton "Lire la suite".
+    readMoreButton.style.display = "block";
+  }
+}
+
+// Commencez l'affichage séquentiel du texte lorsque la page est prête.
+window.addEventListener("load", afficherTexteSequentiel);
+
+// Ajoutez un gestionnaire d'événements pour le bouton "Lire la suite".
